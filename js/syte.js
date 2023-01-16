c94593b8e12e4c32945ce3346c8a8a2474be9d1a
Lucas Santiago Gomez <104328546+LucasSantiagoGomez1@users.noreply.github.com>
2023-01-16 06:57:39 -0300

    Add files via upload

--- js/syte.js
+++ js/syte.js
@@ -11,6 +11,16 @@ console.log("La Dirección Del Cliente Es: " + direccion);
 var preciototal = 0;
 var precioplato = 0;
 
+function acumulador(vacumulador, vacumular, repeticiones){
+    for(let i = 1 ; i <= repeticiones ; i++){
+        vacumular = parseInt(prompt("ingrese el precio del " + i + " plato."));
+        console.log(vacumular);
+        vacumulador += vacumular;
+        console.log(vacumulador);
+    }
+    return vacumulador
+}
+
 if(((nombre == "") || (nombre === null )) || ((dni == "") || (dni === null)) || ((telefono == "") || (telefono === null)) || ((direccion == "") || (direccion === null ))){
 
     alert("Puede Que Algun Dato Le haya Faltado, Intentelo Nuevamente.")
@@ -18,16 +28,11 @@ if(((nombre == "") || (nombre === null )) || ((dni == "") || (dni === null)) || 
 }
 
 else{
-    alert("bienbenido " + nombre + ".")
+    alert("bienvenido " + nombre + ".")
     console.log("correcto");
     let npedidos = prompt("Ingrese Cuantos Platos Va A Pedir, Si Su Total Es Mayor A 600$ Se Realizara Un Pequeño Descuento.")
-    console.log("El numero de pedidos es: " + npedidos)
-    for(let i = 1 ; i <= npedidos ; i++){
-        precioplato = parseInt(prompt("ingrese el precio del " + i + " plato."));
-        console.log(precioplato);
-        preciototal += precioplato;
-        console.log(preciototal);
-    }
+    console.log("El numero de pedidos es: " + npedidos);
+    preciototal=(acumulador(preciototal, precioplato, npedidos));
     if(preciototal <= 600){
         alert("Su precio total es: " + preciototal)
     }
