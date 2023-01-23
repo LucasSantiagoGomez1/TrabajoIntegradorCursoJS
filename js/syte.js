90318edf14aa19486c251a645f513843753bdb04
Lucas Santiago Gomez <104328546+LucasSantiagoGomez1@users.noreply.github.com>
2023-01-23 20:37:51 -0300

    Add files via upload

--- js/syte.js
+++ js/syte.js
@@ -1,25 +1,51 @@
 
-alert("Ingrese Sus Datos");
-let nombre = prompt("ingrese su nombre completo");
-console.log("El Nombre Del Cliente Es: " + nombre);
-let dni = prompt("Igrese Su Numero de DNI");
-console.log("El DNI Del Cliente Es: " + dni);
-let telefono = prompt("Ingrese Su Numero De Telefono");
-console.log("El Telefono Del Cliente Es: " + telefono);
-let direccion = prompt("Ingrese su dirección para la entrega");
-console.log("La Dirección Del Cliente Es: " + direccion);
-var preciototal = 0;
-var precioplato = 0;
+/* funciones ,constructores y clases*/
 
-function acumulador(vacumulador, vacumular, repeticiones){
+class Usuario {
+    constructor (nombre, dni, telefono, dirección){
+        this.nombre = nombre;
+        this.dni = dni;
+        this.telefono = telefono;
+        this.dirección = dirección;
+    }
+    
+    darBienvenida() {
+        alert("bienvenido " + this.nombre + ".")
+    }
+}
+
+class pedido{
+    constructor(id, nombrePlato, precio){
+        this.id = id;
+        this.nombrePlato = nombrePlato;
+        this.precio = precio;
+    }
+
+}
+
+function pedidoCompleto(vacumulador, vacumular, repeticiones){
     for(let i = 1 ; i <= repeticiones ; i++){
+        let nombreP = prompt("ingrese el nombre del " + i + " plato.");
         vacumular = parseInt(prompt("ingrese el precio del " + i + " plato."));
+        pedidos.push(new pedido(i, nombreP, vacumular));
         console.log(vacumular);
         vacumulador += vacumular;
         console.log(vacumulador);
     }
     return vacumulador
 }
+
+/* codigo y el resto*/
+
+alert("Ingrese Sus Datos");
+let nombre = prompt("ingrese su nombre completo");
+let dni = prompt("Igrese Su Numero de DNI");
+let telefono = prompt("Ingrese Su Numero De Telefono");
+let direccion = prompt("Ingrese su dirección para la entrega");
+var preciototal = 0;
+var precioplato = 0;
+const pedidos = [];
+
 
 if(((nombre == "") || (nombre === null )) || ((dni == "") || (dni === null)) || ((telefono == "") || (telefono === null)) || ((direccion == "") || (direccion === null ))){
 
@@ -28,15 +54,26 @@ if(((nombre == "") || (nombre === null )) || ((dni == "") || (dni === null)) || 
 }
 
 else{
-    alert("bienvenido " + nombre + ".")
+
+    const usuario1 = new Usuario(nombre, dni, telefono, direccion);
+    console.log(usuario1);
+    usuario1.darBienvenida();
     console.log("correcto");
     let npedidos = prompt("Ingrese Cuantos Platos Va A Pedir, Si Su Total Es Mayor A 600$ Se Realizara Un Pequeño Descuento.")
     console.log("El numero de pedidos es: " + npedidos);
-    preciototal=(acumulador(preciototal, precioplato, npedidos));
+    preciototal=(pedidoCompleto(preciototal, precioplato, npedidos));
     if(preciototal <= 600){
+        alert("sus pedidos individuales son: ");
+        for(const pedido of pedidos) {
+            alert( "pedido N: " + pedido.id + " || nombre: "  + pedido.nombrePlato +  " ||  precio: " + pedido.precio);
+        }
         alert("Su precio total es: " + preciototal)
     }
     else{
+        alert("sus pedidos individuales son: ");
+        for(const pedido of pedidos) {
+            alert( "pedido N: " + pedido.id + " || nombre: "  + pedido.nombrePlato +  " ||  precio: " + pedido.precio);
+        }
         var descuento = (preciototal * 5)/100;
         var preciodescontado = preciototal - descuento;
         alert("Su precio total con descuento es: " + preciodescontado);
